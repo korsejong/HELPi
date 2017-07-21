b03a7fff830c47c2b78e74c529650ae1947fc623
korsejong <sejong.400@gmail.com>
2017-07-21 22:56:00 +0900

    add gitignore

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 require('./config/passport')(passport);
 require('./config/routes')(app,passport);
+require('./config/share')(app);
 
 const mongoDB = 'mongodb://127.0.0.1:27017/HELPi';
 mongoose.Promise = global.Promise;
